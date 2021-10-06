2d24a7134a7a56db4a2162a45e967adebc8b1035
Trent Green <70169861+Trent325@users.noreply.github.com>
2021-10-06 18:37:01 -0400

    upload 1

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,8 @@
+document.getElementById("id1").innerHTML = "title changed";
+document.getElementById("bossy").innerHTML = "less bossy";
+document.getElementById("description").innerHTML = "Trent Green";
+document.getElementById("one").style.color = "blue";
+document.getElementById("two").style.backgroundColor = "#FFFDD0"; //smh thats cold it barely shows 
+document.body.style.backgroundColor = "grey";
+document.getElementById("three").style.textTransform = "uppercase";
+document.getElementById("four").classList.add("title");
